0ac053c82ece361dbba215de5223e3320b22e6d7
Jolar Tabungar <jolar_jjb24@hotmail.com>
2021-04-17 23:15:39 -0400

    Update UI for tuning controls

--- src/model.py
+++ src/model.py
@@ -1,133 +1,83 @@
 import cv2 as cv
 import numpy as np
+import tuning_constants as tuning
+from skimage.metrics import structural_similarity as ssim
 
 
 class Image:
     def __init__(self):
-        self.cap = cv.VideoCapture("../data/vid1.mp4")
-        self.edge_detector_model = cv.ximgproc.createStructuredEdgeDetection('../detection_model/model.yml')
-        self.frames_total = int(self.cap.get(7) - 1)
-
+        self.video_path = None
+        self.cap = None
         self.frame = 1
-        self.dilate_iteration = 0
-        self.erode_iteration = 0
-        self.thresh1 = 0
-        self.thresh2 = 0
+        self.frames_total = 0
 
-        self.use_canny = False
+        self.set_video_path("../data/bass_tuned_1.mp4")
+        self.edge_detector_model = cv.ximgproc.createStructuredEdgeDetection('../detection_model/model.yml')
+
+        self.show_processed = False
+        self.bounding_box = None
+        self.selected_string = tuning.STRING_E
+
+        self.tuning_detection_active = False
+        self.initial_tuning_frame = None
 
     def set_frame(self, value):
         self.frame = value
 
-    def set_dilate_iteration(self, value):
-        self.dilate_iteration = value
+    def set_video_path(self, path):
+        self.video_path = path
+        self.cap = cv.VideoCapture(self.video_path)
+        self.frames_total = int(self.cap.get(7) - 1)
 
-    def set_erode_iteration(self, value):
-        self.erode_iteration = value
+    def set_bounding_box(self, box):
+        self.bounding_box = box
+        print(self.bounding_box)
 
-    def set_thresh1(self, value):
-        self.thresh1 = value
+    def set_selected_string(self, string):
+        self.selected_string = string
 
-    def set_thresh2(self, value):
-        self.thresh2 = value
+    def start_tuning_detection(self):
+        if self.bounding_box is not None:
+            self.tuning_detection_active = False
+            self.initial_tuning_frame = self.retrieve_cropped_frame(self.get_image())
+            self.tuning_detection_active = True
+        else:
+            print("ERROR: No bounding box found!")
 
-    def edge_detect_canny(self, image):
-        # gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        # _, thresh = cv.threshold(gray_image, 30, 255, cv.THRESH_BINARY)
+    def retrieve_cropped_frame(self, img):
+        p1, p2 = self.bounding_box
+        cropped = img[int(p1.y()):int(p2.y()), int(p1.x()):int(p2.x())]
+        return cropped
 
-        # kernel_sharpen = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
-        # morph = cv.morphologyEx(image, cv.MORPH_OPEN, kernel_sharpen)
-        #
-        # result_morph = morph.copy()
-        # contours = cv.findContours(morph, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        # contours = contours[0] if len(contours) == 2 else contours[1]
-        # for c in contours:
-        #     area = cv.contourArea(c)
-        #     if area < 100:
-        #         cv.drawContours(result_morph, [c], 0, (0, 0, 0), -1)
+    def compare_frames(self, curr_frame):
+        initial = cv.cvtColor(self.initial_tuning_frame, cv.COLOR_BGR2GRAY)
+        curr = cv.cvtColor(curr_frame, cv.COLOR_BGR2GRAY)
 
-        result_canny = cv.Canny(image, self.thresh1, self.thresh2, apertureSize=3, L2gradient=False)
-        return result_canny
+        s = ssim(initial, curr)
+        print(s)
+
+    def detected_tuning(self):
+        return '> ' + self.selected_string
 
     def edge_detect_trained(self, image):
         rgb_image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         float32_image = rgb_image.astype(np.float32) / 255.0
         edges = self.edge_detector_model.detectEdges(float32_image) * 255.0
+
         return np.clip(edges, 0, 255).astype(np.uint8)
 
-    def remove_salt_pepper_noise(self, image):
-        src = image.copy()
-        count = 0
-        last_median = src
-        median = cv.medianBlur(src, 3)
-
-        while not np.array_equal(last_median, median):
-            zeroed = np.invert(np.logical_and(median, src))
-            src[zeroed] = 0
-            count = count + 1
-            if count > 70:
-                break
-
-            last_median = median
-            median = cv.medianBlur(src, 3)
-
-        return src
-    
-    def find_significant_contours(self, image):
-        contours, hierarchy = cv.findContours(
-            image,
-            cv.RETR_TREE,
-            cv.CHAIN_APPROX_SIMPLE
-        )
-
-        contours_to_draw = contours
-
-        if hierarchy is not None:
-            lvl_1_contours = []
-            for contour_index, _tuple in enumerate(hierarchy[0]):
-                if _tuple[3] != -1:
-                    _tuple = np.insert(_tuple.copy(), 0, [contour_index])
-                    lvl_1_contours.append(_tuple)
-                
-            contours_with_area = []
-            for _tuple in lvl_1_contours:
-                contour_index = _tuple[0]
-                contour = contours[contour_index]
-                area = cv.contourArea(contour)
-                contours_with_area.append(contour)
-
-            contours_to_draw = contours_with_area
-
-        result_contours = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
-        for i in range(len(contours_to_draw)):
-            cv.drawContours(result_contours, contours_to_draw, i, (255, 255, 255), 2, cv.LINE_AA, maxLevel=1)
-        
-        return result_contours
-
     def isolate_strings(self, original_image):
-        gray_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
         result_gaussian_blur = cv.GaussianBlur(original_image, (5, 5), 0)
+        result_edge = self.edge_detect_trained(result_gaussian_blur)
 
-        result_edge = self.edge_detect_canny(result_gaussian_blur) \
-            if self.use_canny else self.edge_detect_trained(result_gaussian_blur)
-
-        # Kernel for erode/dilate
-        kernel_erode_dilate = np.ones((2, 2), np.uint8)
-
-        result_erode_dilate = result_edge
-        if self.dilate_iteration != 0:
-            result_erode_dilate = cv.dilate(result_erode_dilate, kernel_erode_dilate, iterations=self.dilate_iteration)
-
-        if self.erode_iteration != 0:
-            result_erode_dilate = cv.erode(result_erode_dilate, kernel_erode_dilate, iterations=self.erode_iteration)
-
-        result_contours = self.find_significant_contours(result_erode_dilate)
-
-        return result_erode_dilate
+        return result_edge
 
     def get_image(self):
         self.cap.set(1, self.frame)
         _flag, img = self.cap.read()
-        img = cv.resize(img[600:1920, 0:1080], (1056, 594))
+        # img = cv.resize(img[600:1920, 0:1080], (1056, 594))
 
-        return self.isolate_strings(img)
+        if self.tuning_detection_active:
+            self.compare_frames(self.retrieve_cropped_frame(img))
+
+        return self.isolate_strings(img) if self.show_processed else img
